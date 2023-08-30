50c3a961b0b3abfa24ae242fd6a0c519bb6bb0e3
liminghui <3209498857@qq.com>
2023-08-30 14:40:06 +0800

    修改头像

--- src/api/user.js
+++ src/api/user.js
@@ -12,3 +12,6 @@ export const userGetInfoService = () => request.get('/my/userinfo')
 // 修改用户个人资料
 export const userUpdateInfoService = ({ id, nickname, email }) =>
   request.put('/my/userinfo', { id, nickname, email })
+//上传头像
+export const userUploadAvatarService = (avatar) =>
+  request.patch('/my/update/avatar', { avatar })
